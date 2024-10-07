6306214a2d701d84468105c39a7508739ed0ec03
Aiden Powers <powersajbusiness@protonmail.com>
2024-10-07 09:35:32 -0700

    Yup

--- Broadway/Main_v0.py
+++ Broadway/Main_v0.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
+    #Start zipline graphics
     import Zipline_v2
     Zipline_v2.begin_thread()
+
     # Dashboard is a GUI tool, should not auto start.
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Paradox for Deadlock
 Deadlock broadcast management
+
+## Current Layout
+- [Broadway: Server](Broadway)
+- [Mirage: Graphics](Mirage) 
+- [Patron: Assets](Patron)
