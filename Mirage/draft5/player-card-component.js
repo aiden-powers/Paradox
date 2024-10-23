5411edd8412e8846981d1e294b0b90a2ea593bff
Aiden <powersaiden07@gmail.com>
2024-10-22 20:39:04 -0700

    yup

--- Mirage/draft5/player-card-component.js
+++ Mirage/draft5/player-card-component.js
@@ -1,34 +1,46 @@
 class PlayerCardComponent extends HTMLElement {
+    static attributesMap = {
+        state: 'updateState', //text: 'updateText',
+        character: 'character', username: 'username', transition: 'updateTransition',
+    };
+
     constructor() {
         super();
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-
+        const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.innerHTML = `
             <link rel="stylesheet" href="player-card.css">
-            <style>
-                .move100 {
-                    width: 256px;
-                    height: 128px;
-                }
-            </style>
+                <style>
+                    .move100 {
+                        width: 256px;
+                        height: 128px;
+                    }
+                </style>
             <div class="player_card_base" id="playerCard">
                 <p class="player_card_text_character" id="character"> Character </p>
-            <p class="player_card_text_username" id="username"> Username </p>
+                <p class="player_card_text_username" id="username"> Username </p>
             </div>
         `;
     }
 
-    static attributesMap = {
-        state: 'updateState',
-        //text: 'updateText',
-        character: 'character',
-        username: 'username',
-        transition: 'updateTransition',
-    };
+    static get observedAttributes() {
+        return Object.keys(PlayerCardComponent.attributesMap);
+    }
 
-    static get observedAttributes() {return Object.keys(PlayerCardComponent.attributesMap);}
-    attributeChangedCallback(name, oldValue, newValue) {const method = PlayerCardComponent.attributesMap[name];if (method && typeof this[method] === 'function') {this[method](newValue);}}
-    connectedCallback() {Object.keys(PlayerCardComponent.attributesMap).forEach((attr) => {if (this.hasAttribute(attr)) {const method = PlayerCardComponent.attributesMap[attr];this[method](this.getAttribute(attr));}})}
+    attributeChangedCallback(name, oldValue, newValue) {
+        const method = PlayerCardComponent.attributesMap[name];
+        if (method && typeof this[method] === 'function') {
+            this[method](newValue);
+        }
+    }
+
+    connectedCallback() {
+        Object.keys(PlayerCardComponent.attributesMap).forEach((attr) => {
+            if (this.hasAttribute(attr)) {
+                const method = PlayerCardComponent.attributesMap[attr];
+                this[method](this.getAttribute(attr));
+            }
+        })
+    }
 
     updateState(newState) {
         console.log('updateState');
@@ -36,8 +48,16 @@ class PlayerCardComponent extends HTMLElement {
         const element = this.shadowRoot.querySelector('#playerCard');
         const stateIndicator = newState.slice(-2); // _1 true, _0 false
         const stateName = newState.slice(0, -2); // name excluding indicator
-        if (stateIndicator === '_1') {if (!element.classList.contains(stateName)) {element.classList.add(stateName);}} // stateName does not already exist, execute addition
-        if (stateIndicator === '_0') {if (element.classList.contains(stateName)) {element.classList.remove(stateName);}} // stateName exists, execute removal
+        if (stateIndicator === '_1') {
+            if (!element.classList.contains(stateName)) {
+                element.classList.add(stateName);
+            }
+        } // stateName does not already exist, execute addition
+        if (stateIndicator === '_0') {
+            if (element.classList.contains(stateName)) {
+                element.classList.remove(stateName);
+            }
+        } // stateName exists, execute removal
     }
 
     updateTransition(newTransition) {
---
+++ Mirage/draft6/states.js
@@ -0,0 +1,10 @@
+const states = {
+    0: {
+        group1: { x: 0, y: 0, rotation: 0, duration: 1, ease: "power1.inOut" },
+        group2: { x: 0, y: 0, rotation: 0, duration: 1, ease: "power1.inOut" }
+    },
+    1: {
+        group1: { x: 300, y: 200, rotation: 45, duration: 1, ease: "power1.inOut" },
+        group2: { x: -200, y: 100, rotation: -45, duration: 1, ease: "power1.inOut" }
+    }
+};
